cf6b10de7eae351b51f377c65ba56b711b579fe4
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2020-12-11 02:27:37 +0100

    MyApp commit -Buttons

--- app/src/main/java/com/di/myapp/MainActivity2.java
+++ app/src/main/java/com/di/myapp/MainActivity2.java
@@ -2,7 +2,9 @@ package com.di.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void volver(View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+        finish();
     }
 }
