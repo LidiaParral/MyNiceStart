583a809c9926e1806bf66c4c887a2a0e098a8128
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2021-02-06 03:43:32 +0100

    Update project
    -Add menu_context.xml and menu_appbar.xml
    -Add activity_main.xml
    -Update strings.xml
    -Update MainActivity.class

--- app/src/main/java/com/di/myapp/MainActivity.java
+++ app/src/main/java/com/di/myapp/MainActivity.java
@@ -1,51 +1,93 @@
 package com.di.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    int contador = 0;
-
-
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.myapp_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        ImageView mycontext = findViewById(R.id.imgProfile);
+        registerForContextMenu(mycontext);
+
+
     }
+    
 
     public void ok(View view) {
         Intent i = new Intent(this, MainActivity2.class);
         startActivity(i);
     }
-//Pesionar el botón retorno del dispositivo para salir de la app: NO funciona*
+
     @Override
-    public void onBackPressed() {
-        if(contador == 0){
-            Toast.makeText(getApplicationContext(),"Saliendo de la aplicación",Toast.LENGTH_LONG);
-            contador++;
-        } else{
-            super.onBackPressed();
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()){
+            case R.id.delete:
+                Toast.makeText(this, "going CONTEXT DELETE",
+                        Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.edit:
+                Toast.makeText(this, "going CONTEXT EDIT",
+                        Toast.LENGTH_LONG).show();
+                break;
         }
+        return super.onContextItemSelected(item);
+    }
 
-        new CountDownTimer(3000,1000){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
 
-            @Override
-            public void onTick(long millisUntilFinished) {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
-            }
-
-            @Override
-            public void onFinish() {
-                contador = 0;
-            }
-        }.start();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        if (id == R.id.camera){
+            Toast.makeText(this,"going APPBAR CAMERA",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
